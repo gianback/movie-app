7218b3f04826c7f7810efbabc72349d35e1c7741
Gian Franco <gianfraback@gmail.com>
2023-06-21 22:33:23 -0500

    refactor(server): axio services removed

--- server/build/routes/comments.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const comments_controller_1 = require("../controllers/comments.controller");
-const router = (0, express_1.Router)();
-router.post("/comments", comments_controller_1.createComment);
-exports.default = router;
--- server/services/index.ts
+++ server/services/index.ts
@@ -1,5 +1,4 @@
 export * from "./auth";
-export * from "./axios.services";
 export * from "./bycript";
 export * from "./cloudinary";
 export * from "./jwt";
